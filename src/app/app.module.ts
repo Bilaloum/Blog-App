0f97925963f3fd2fd7efee1ccf1e1aa3befbba15
bilaloum <bilaloumehdi01@gmail.com>
2023-02-24 01:13:25 +0100

    create the components of pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,31 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { CategoryNavBarComponent } from './layouts/category-nav-bar/category-nav-bar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { TermsAndConditionsComponent } from './pages/terms-and-conditions/terms-and-conditions.component';
+import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
+import { CommentFormComponent } from './comments/comment-form/comment-form.component';
+import { CommentListComponent } from './comments/comment-list/comment-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    CategoryNavBarComponent
+    CategoryNavBarComponent,
+    HomeComponent,
+    SingleCategoryComponent,
+    SinglePostComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    TermsAndConditionsComponent,
+    SubscriptionFormComponent,
+    CommentFormComponent,
+    CommentListComponent
   ],
   imports: [
     BrowserModule,
