5e4e7c0bf2b8104a069a823f185137fd3e9ae60e
bilaloum <bilaloumehdi01@gmail.com>
2023-02-24 01:04:43 +0100

    create layout componnets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { CategoryNavBarComponent } from './layouts/category-nav-bar/category-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CategoryNavBarComponent
   ],
   imports: [
     BrowserModule,
